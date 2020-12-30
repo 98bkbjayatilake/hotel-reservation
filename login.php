44698c275ad7611e19a88e88a229a1f899244a27
rkskrajapakshe <74091350+rkskrajapakshe@users.noreply.github.com>
2020-12-30 16:17:05 +0530

    connected registration to database

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php
-
 $con=mysqli_connect("localhost","root","","hotel");
 mysqli_select_db($con,"hotel");
 session_start();
     $name    = $_POST['uname2'];
     $password = $_POST['passu'];
-    $result = mysqli_query($mysqli, "select name,password from register
-        where name='$name' and password='$password'");
-    $user_matched = mysqli_num_rows($result);
-    if ($user_matched > 0) {
+$sql="select * from register where name='$name' && password='$password'";
+    $result = mysqli_query($con,$sql);
+   $num=mysqli_num_rows($result);
+if($num==1){
+echo "<script>alert('welcome, successfully logged in!');window.location='index.html'</script>";
         header("location:index.html");
-    } else {
+ } else {
         header("location:error.html");
     }
 ?>
--- register.php
+++ register.php
@@ -6,6 +6,16 @@ $name=$_POST['uname'];
 $email=$_POST['email'];
 $pass=$_POST['pass1'];
 $pass2=$_POST['pass2'];
-$sql="insert into register (name,email,password,confirmed_password) values('$name','$email','$pass','$pass2');";
-mysqli_query($con,$sql);
+$sql="select * from register where name='$name'";
+$res=mysqli_query($con,$sql);
+$num=mysqli_num_rows($res);
+if($num==1){
+echo "<script>alert('this user name already exists');window.location='registration.html'</script>";
+}
+else{
+$string="insert into register(name,email,password,confirmed_password) values('$name','$email','$pass','$pass2')";
+$result=mysqli_query($con,$string);
+echo "<p><script>alert('you have successfully registerd');window.location='registration.html'</script>;</p>";
+}
+mysqli_close($con);
 ?>
